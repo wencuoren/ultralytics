c1fee5977e75e761e5aed79a21553418287bbdf6
Laughing-q <1185102784@qq.com>
2026-04-03 15:33:41 +0800

    Update export/torchscript.py

--- ultralytics/utils/export/torchscript.py
+++ ultralytics/utils/export/torchscript.py
@@ -29,11 +29,11 @@ def torch2torchscript(
         prefix (str): Prefix for log messages.
 
     Returns:
-        (Path): Path to the exported ``.torchscript`` file.
+        (str): Path to the exported ``.torchscript`` file.
     """
     LOGGER.info(f"\n{prefix} starting export with torch {TORCH_VERSION}...")
-    file = Path(file)
-    f = file.with_suffix(".torchscript")
+    if not str(file).endswith(".torchscript"):
+        file = str(Path(file).with_suffix(".torchscript"))
 
     ts = torch.jit.trace(model, im, strict=False)
     extra_files = {"config.txt": json.dumps(metadata or {})}  # torch._C.ExtraFilesMap()
@@ -41,7 +41,7 @@ def torch2torchscript(
         LOGGER.info(f"{prefix} optimizing for mobile...")
         from torch.utils.mobile_optimizer import optimize_for_mobile
 
-        optimize_for_mobile(ts)._save_for_lite_interpreter(str(f), _extra_files=extra_files)
+        optimize_for_mobile(ts)._save_for_lite_interpreter(file, _extra_files=extra_files)
     else:
-        ts.save(str(f), _extra_files=extra_files)
-    return f
+        ts.save(file, _extra_files=extra_files)
+    return file
